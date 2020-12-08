64a9cb79e71a28b970754a42f5b224f5719cdf6b
Sky <sky_py1044@hotmail.com>
2020-12-08 17:13:46 +0800

    更新Controller

--- sheep-paper/src/main/java/sheep/paper/Controller/PaperController.java
+++ sheep-paper/src/main/java/sheep/paper/Controller/PaperController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sheep.paper.Entity.Paper;
 import sheep.paper.Repository.PaperRepository;
 import sheep.paper.Service.PaperService;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,23 +24,61 @@ public class PaperController {
     @Autowired
     private PaperService paperService;
 
-    @RequestMapping("/info/{paperId}")
-    public String getPaperInfo(@PathVariable int paperId) {
-        return paperRepository.findPaperByPaperId(paperId).toString();
+//    查询 MySQL 中的全部项
+    @GetMapping("/info/mysql/{paperIdStr}")
+    public Paper getMySqlInfoById(@PathVariable String paperIdStr) {
+        int paperId;
+        try {
+            paperId = Integer.parseInt(paperIdStr);
+        } catch (Exception e) {
+            return null;
+        }
+        return paperRepository.findPaperByPaperId(paperId);
     }
 
-    @GetMapping("/detail/{paperId}")
-    public String getPaperDetailById(@PathVariable String paperId) {
-        GetRequest getRequest = new GetRequest("paper", paperId);
+//    查询 ElasticSearch 中的全部项
+    @GetMapping("/info/es/{paperIdStr}")
+    public Map<String, Object> getEsInfoById(@PathVariable String paperIdStr) {
+        GetRequest getRequest = new GetRequest("paper", paperIdStr);
         try {
             GetResponse response = paperService.getDetail(getRequest);
-            return response.getSourceAsString();
+            return response.getSourceAsMap();
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
     }
 
+//    获取 paper 的完整信息
+    @GetMapping("/info/full/{paperIdStr}")
+    public Paper getFullInfoById(@PathVariable String paperIdStr) {
+        Paper paper = this.getMySqlInfoById(paperIdStr);
+        Map<String, Object> responseMap = this.getEsInfoById(paperIdStr);
+        if (paper == null && responseMap==null) {
+            return null;
+        }
+        else if (responseMap==null) {
+            return paper;
+        }
+        else {
+            if (paper==null) {
+                paper = new Paper();
+            }
+            paper.setPaperAbstract((String) responseMap.get("abstract"));
+            paper.setAuthorName((String) ((Map<String, Object>) responseMap.get("author")).get("name"));
+            paper.setAuthorOrg((String) ((Map<String, Object>) responseMap.get("author")).get("org"));
+            paper.setFieldOfStudy((String) responseMap.get("fos"));
+            paper.setKeywords((String) responseMap.get("keywords"));
+            paper.setnCitation((Integer) responseMap.get("n_citation"));
+            paper.setTitle((String) responseMap.get("title"));
+            paper.setUrl((String) responseMap.get("url"));
+            paper.setVenue((String) responseMap.get("venue"));
+            paper.setYear((Integer) responseMap.get("year"));
+            return paper;
+        }
+    }
+
+//    获取引文格式
     @GetMapping("/ref/{paperId}")
     public String gerRefString(@PathVariable String paperId) {
         GetRequest getRequest = new GetRequest("paper", paperId);
--- sheep-paper/src/main/java/sheep/paper/Entity/Paper.java
+++ sheep-paper/src/main/java/sheep/paper/Entity/Paper.java
@@ -1,10 +1,8 @@
 package sheep.paper.Entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Table(name = "paper_info")
@@ -26,4 +24,196 @@ public class Paper implements Serializable {
     private String doi;
     @Column(name = "pdf")
     private String pdfLink;
+
+//    Data in ElasticSearch
+    @Transient
+    private String paperAbstract;
+//    TODO 多作者？
+    @Transient
+    private String authorName;
+    @Transient
+    private String authorOrg;
+    @Transient
+    private String fieldOfStudy;
+//    TODO keywords 应该改为 List<String>
+    @Transient
+    private String keywords;
+    @Transient
+    private int nCitation;
+    @Transient
+    private String title;
+    @Transient
+    private String url;
+    @Transient
+    private String venue;
+    @Transient
+    private int year;
+
+    public int getPaperId() {
+        return paperId;
+    }
+
+    public void setPaperId(int paperId) {
+        this.paperId = paperId;
+    }
+
+    public int getPaperStat() {
+        return paperStat;
+    }
+
+    public void setPaperStat(int paperStat) {
+        this.paperStat = paperStat;
+    }
+
+    public int getPaperEnd() {
+        return paperEnd;
+    }
+
+    public void setPaperEnd(int paperEnd) {
+        this.paperEnd = paperEnd;
+    }
+
+    public int getDocType() {
+        return docType;
+    }
+
+    public void setDocType(int docType) {
+        this.docType = docType;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getIssue() {
+        return issue;
+    }
+
+    public void setIssue(int issue) {
+        this.issue = issue;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getDoi() {
+        return doi;
+    }
+
+    public void setDoi(String doi) {
+        this.doi = doi;
+    }
+
+    public String getPdfLink() {
+        return pdfLink;
+    }
+
+    public void setPdfLink(String pdfLink) {
+        this.pdfLink = pdfLink;
+    }
+
+    public String getPaperAbstract() {
+        return paperAbstract;
+    }
+
+    public void setPaperAbstract(String paperAbstract) {
+        this.paperAbstract = paperAbstract;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorOrg() {
+        return authorOrg;
+    }
+
+    public void setAuthorOrg(String authorOrg) {
+        this.authorOrg = authorOrg;
+    }
+
+    public String getFieldOfStudy() {
+        return fieldOfStudy;
+    }
+
+    public void setFieldOfStudy(String fieldOfStudy) {
+        this.fieldOfStudy = fieldOfStudy;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public int getnCitation() {
+        return nCitation;
+    }
+
+    public void setnCitation(int nCitation) {
+        this.nCitation = nCitation;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
