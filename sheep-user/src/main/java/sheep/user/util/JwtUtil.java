b7712ec40e02910c7ab518429b337b44e90aa893
St2r <St2r@github.com>
2020-12-17 22:27:01 +0800

    :beers: add login and auth

---
+++ sheep-user/src/main/java/sheep/user/util/JwtUtil.java
@@ -0,0 +1,18 @@
+package sheep.user.util;
+
+public class JwtUtil {
+    public static String generatorToken() {
+        // todo
+        return "wtf";
+    }
+
+    public static boolean verifyToken(String token) {
+        // todo
+        return true;
+    }
+
+    public static String parseUserId(String token) {
+        // todo
+        return "user";
+    }
+}
