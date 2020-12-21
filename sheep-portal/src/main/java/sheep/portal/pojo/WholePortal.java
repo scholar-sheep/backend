2ae025c377a8f140006a6243788486e0534b6c4d
Zoeyzzzzzz <798804629@qq.com>
2020-12-21 23:25:32 +0800

    delete portal's mysql

--- sheep-portal/src/main/java/sheep/portal/pojo/WholePortal.java
+++ sheep-portal/src/main/java/sheep/portal/pojo/WholePortal.java
@@ -3,7 +3,6 @@ package sheep.portal.pojo;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import sheep.portal.entity.EsPortal;
-import sheep.portal.entity.Portal;
 
 import java.util.List;
 
@@ -13,12 +12,12 @@ public class WholePortal<T>{
     private String portal_id;
     //学者姓名
     private String name;
-    //学者规范化姓名，如"Huibin Xu"
-    private String normalizedName;
-    //该学者的职称，例如教授
-    private String position;
-    //该学者的h指数
-    private int hIndex;
+//    //学者规范化姓名，如"Huibin Xu"
+//    private String normalizedName;
+//    //该学者的职称，例如教授
+//    private String position;
+//    //该学者的h指数
+//    private int hIndex;
     //记录该作者的所属机构
     private String org;
     //学者发布的学术成果数量
@@ -26,18 +25,17 @@ public class WholePortal<T>{
     //学者的学术成果被引用次数
     private int n_citation;
     private List<EsPortal.Tag> tags;
+    private List<EsPortal.Pub>pubs;
     private List<T> paperList;
 
-    public WholePortal(Portal mysql, EsPortal es,List<T> paperList){
-        this.portal_id = mysql.getId();
-        this.name = mysql.getName();
-        this.normalizedName = mysql.getNormalizedName();
-        this.position = mysql.getPosition();
-        this.hIndex = mysql.getHIndex();
+    public WholePortal(EsPortal es, List<T> paperList){
+        this.portal_id = es.getId();
+        this.name = es.getName();
         this.org = es.getOrg();
         this.n_pubs = es.getN_pubs();
         this.n_citation = es.getN_citation();
         this.tags=es.getTags();
+        this.pubs = es.getPubs();
         this.paperList=paperList;
     }
 }
