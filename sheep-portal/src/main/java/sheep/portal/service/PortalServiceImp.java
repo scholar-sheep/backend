f3bb0108e6b6e6259a53627847e77a42ae5351d1
Zoeyzzzzzz <52778162+Zoeyzzzzzz@users.noreply.github.com>
2020-12-23 18:05:10 +0800

    Update PortalServiceImp.java

--- sheep-portal/src/main/java/sheep/portal/service/PortalServiceImp.java
+++ sheep-portal/src/main/java/sheep/portal/service/PortalServiceImp.java
@@ -1,118 +1,209 @@
 package sheep.portal.service;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.alibaba.fastjson.JSON;
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import sheep.portal.entity.Follow;
-import sheep.portal.entity.PortalAndUser;
-import sheep.portal.exception.AdoptFailException;
-import sheep.portal.exception.FollowFailException;
-import sheep.portal.exception.NoPortalException;
-import sheep.portal.mapper.FollowMapper;
-import sheep.portal.mapper.PortalAndUserMapper;
+import org.springframework.util.StringUtils;
+import sheep.portal.pojo.PaperParam;
+import sheep.portal.util.*;
+import sheep.portal.entity.EsPortal;
+import sheep.portal.mapper.EsPortalMapper;
+import org.elasticsearch.client.RestHighLevelClient;
+import sheep.portal.pojo.PaperList;
+import sheep.portal.pojo.PaperModel;
+
+import javax.validation.constraints.Null;
+import java.io.IOException;
+import java.util.*;
+
+
 
 @Service
-public class PortalServiceImp implements PortalService{
+public class EsPortalServiceImp implements EsPortalService{
+
+    @Qualifier("elasticsearchClient")
+    @Autowired
+    RestHighLevelClient highLevelClient;
 
     @Autowired
-    PortalAndUserMapper portalAndUserMapper;
+    private RedisTemplate<String, Object> redisTemplate;
+    @Autowired
+    private  RedisUtil redisUtil;
 
     @Autowired
-    FollowMapper followMapper;
+    EsPortalMapper esPortalMapper;
 
+    @Override
+    public void save(EsPortal esPortal){
 
-    /**
-     * 认领门户，传入被认领的门户id和申请认领的用户id
-     * @param portal_id
-     * @param user_id
-     * @return
-     */
-    public void adoptPortal (String portal_id, int user_id){
-        //该门户有没有被认领过
-        int count1 = portalAndUserMapper.selectCount(new QueryWrapper<PortalAndUser>().eq("portal_id", portal_id));
-        //该用户有没有认领过门户
-        int count2 = portalAndUserMapper.selectCount(new QueryWrapper<PortalAndUser>().eq("user_id", user_id));
-        //如果以上两种情况有一个发生，则认领失败
-        if(count1 > 0 || count2 > 0) throw new AdoptFailException();
-        portalAndUserMapper.adoptPortal(portal_id, user_id);
+        esPortalMapper.save(esPortal);
     }
 
-    /**
-     * 取消认领门户，传入被取消门户的id
-     * @param portal_id
-     */
-    public void unadoptPortal(String portal_id){
-        //该门户有没有被认领过
-        int count = portalAndUserMapper.selectCount(new QueryWrapper<PortalAndUser>().eq("portal_id", portal_id));
-        if(count != 1) throw new AdoptFailException();
-        portalAndUserMapper.unadoptPortal(portal_id);
+    @Override
+    //删除失败返回2
+    //成功返回1
+    public int deleteByID(String id) throws IOException{
+        DeleteRequest deleteRequest = new DeleteRequest("sheep-scholar",id);
+        DeleteResponse deleteResponse = highLevelClient.delete(
+                deleteRequest, RequestOptions.DEFAULT);
+        if (deleteResponse.getResult() == DocWriteResponse.Result.NOT_FOUND) {
+            return 2;
+        }
+        return 1;
     }
 
-    /**
-     * 查询门户是否被用户认领
-     * @param portal_id
-     * @param user_id
-     * @return 0：没有被认领；1：被正常认领
-     */
-    public int isAdopt(String portal_id, int user_id){
-        int count = portalAndUserMapper.selectCount(new QueryWrapper<PortalAndUser>().eq("portal_id", portal_id).eq("user_id", user_id));
-        if(count == 0) return 0;
-        else return count;
+    @Override
+    //成功更新返回1
+    //无更新返回2
+    //更新失败返回3
+    public int update(String id, EsPortal esPortal) throws IOException {
+        //构建改的hashmap
+        Map<String, Object> jsonMap = new HashMap<>();
+        if(esPortal.getName() != null) jsonMap.put("name", esPortal.getName());
+        if(esPortal.getOrg() != null) jsonMap.put("org", esPortal.getOrg());
+        if(esPortal.getN_pubs() != 0) jsonMap.put("n_pubs", esPortal.getN_pubs());
+        if(esPortal.getN_citation() != 0) jsonMap.put("n_citation", esPortal.getN_citation());
+        if(esPortal.getTags()!= null) jsonMap.put("tags_t", esPortal.getTags());
+        //构建updateRequest
+        UpdateRequest updateRequest = new UpdateRequest("sheep-scholar", "_doc", id);
+        updateRequest.doc(jsonMap);
+        UpdateResponse updateResponse = highLevelClient.update(updateRequest,RequestOptions.DEFAULT);
+        if (updateResponse.getResult() == DocWriteResponse.Result.UPDATED)
+            return 1;
+        else if (updateResponse.getResult() == DocWriteResponse.Result.NOOP)
+            return 2;
+        else return 0;
     }
 
-    /**
-     * 根据用户id查找门户id
-     * @param user_id
-     */
-    public String findPortalByUserId(int user_id)
+    @Override
+    public EsPortal getInformation(String id) throws IOException {
+        GetRequest getRequest = new GetRequest("sheep-scholar",id);
+        GetResponse response =  highLevelClient.get(getRequest, RequestOptions.DEFAULT);
+        String sourceAsString = response.getSourceAsString();
+        EsPortal esPortal= JSON.parseObject(sourceAsString, EsPortal.class);
+        esPortal.setId(id);
+        return esPortal;
+    }
+
+
+    public PaperModel getPaperDetail(String id) throws IOException {
+        GetRequest getRequest = new GetRequest("sheep-paper-test", id);
+        GetResponse response =  highLevelClient.get(getRequest, RequestOptions.DEFAULT);
+        String sourceAsString = response.getSourceAsString();
+        PaperModel paperModel= JSON.parseObject(sourceAsString, PaperModel.class);
+        //扁平化作者列表
+        StringBuilder sb=new StringBuilder();
+        if(paperModel!=null){
+        List<PaperModel.Author> authors=paperModel.getAuthors();
+        if(authors!=null) {
+            for (PaperModel.Author author : authors) {
+                sb.append(author.getName());
+                sb.append(",");
+            }
+            if(sb.length()!=0)
+                sb.deleteCharAt(sb.length() - 1);
+            paperModel.setAuthorNames(sb.toString());
+        }
+        PaperModel.Venue venue=paperModel.getVenue();
+        //提取刊物名
+        if(venue!= null)
+        {
+            paperModel.setVenueName(venue.getRaw());
+        }}
+        return paperModel;
+    }
+    @Override
+    public void setPaperList(String id) throws IOException {
+        GetRequest getRequest = new GetRequest("sheep-scholar", id);
+        GetResponse response =  highLevelClient.get(getRequest, RequestOptions.DEFAULT);
+        String sourceAsString = response.getSourceAsString();
+        EsPortal esPortal= JSON.parseObject(sourceAsString, EsPortal.class);
+        List<EsPortal.Pub> pubs=esPortal.getPubs();
+        //封装学者的论文列表
+        if(pubs!=null)
+        {
+            for (EsPortal.Pub pub : pubs) {
+                PaperModel paperModel = this.getPaperDetail(pub.getI());
+                redisUtil.lSet(id, paperModel);
+            }
+        }
+
+    }
+
+    @Override
+    public List<PaperModel> getPaperList(String id, String sort) throws IOException
     {
-        int count = portalAndUserMapper.selectCount(new QueryWrapper<PortalAndUser>().eq("user_id", user_id));
-        if(count != 1) throw new NoPortalException();
-      return portalAndUserMapper.findByUser_id(user_id);
+        //若redis中不存在则先存入
+        if(!redisUtil.hasKey(id))
+            this.setPaperList(id);
+        PaperList paperList = new PaperList();
+        List<PaperModel> list = (List)redisUtil.sort(id, id+"->", sort);
+        return list;
     }
 
-    /**
-     * 用户关注学者
-     * @param portal_id
-     * @param user_id
-     */
-    public void follow(String portal_id, int user_id){
-        if(isFollow(portal_id, user_id) != 0 || isAdopt(portal_id, user_id) != 0)
-            throw new FollowFailException();
-        followMapper.follow(portal_id, user_id);
+    @Override
+    //失败添加返回0
+    //成功添加返回1
+    //重复添加返回2
+    public int addPaper(String portal_id, String paper_id) throws IOException
+    {
+        PaperModel paperModel=this.getPaperDetail(paper_id);
+        List<Object>paperModels=redisUtil.lGet(portal_id,0,-1);
+        for(Object item:paperModels)
+        {
+            PaperModel paper=(PaperModel)item;
+            if(paper.getId().equals(paper_id)){
+                return 2;
+            }
+        }
+//        updateNpubs(1, portal_id);
+        return redisUtil.lSet(portal_id,paperModel)==true?1:0;
+    }
+    @Override
+    // 0删除论文失败 1删除论文成功
+    public int deletePaper(String portal_id,String paper_id) throws  IOException
+    {
+        List<Object> paperModels=redisUtil.lGet(portal_id,0,-1);
+        for(Object item:paperModels)
+        {
+            PaperModel paper=(PaperModel)item;
+            if(paper.getId().equals(paper_id)){
+//                updateNpubs(-1, portal_id);
+                return redisUtil.lRemove(portal_id,0,item)>0?1:0;
+            }
+        }
+        return 0;
+    }
+    @Override
+    public int createPaper(String portal_id,PaperParam paperParam) throws IOException {
+        PaperModel newPaper= new PaperModel(paperParam);
+//        updateNpubs(1, portal_id);
+        return redisUtil.lSet(portal_id, newPaper) == true ? 1 : 0;
     }
 
-    /**
-     * 用户取消关注学者
-     * @param portal_id
-     * @param user_id
-     */
-    public void unfollow(String portal_id, int user_id){
-        if(isFollow(portal_id, user_id) != 1)
-            throw new FollowFailException();
-        followMapper.unfollow(portal_id, user_id);
-    }
-
-    /**
-     * 查询该用户是否已关注该学者
-     * @param portal_id
-     * @param user_id
-     * @return 0：没有关注；1：正常关注
-     */
-    public int isFollow(String portal_id, int user_id){
-//        int count = followMapper.isFollow(portal_id, user_id);
-        int count = followMapper.selectCount(new QueryWrapper<Follow>().eq("portal_id", portal_id).eq("user_id", user_id));
-        if(count == 0) return 0;
-        else return count;
-    }
-
-    /**
-     * 查询门户被关注次数
-     * @param portal_id
-     * @return
-     */
-    public int followNum(String portal_id){
-        return followMapper.followNum(portal_id);
-    }
-
+//    //在删除和增加论文时修改门户的n_pubs
+//    public void updateNpubs(int num, String portal_id) throws IOException {
+//        GetRequest getRequest = new GetRequest("sheep-scholar", portal_id);
+//        GetResponse response =  highLevelClient.get(getRequest, RequestOptions.DEFAULT);
+//        String sourceAsString = response.getSourceAsString();
+//        EsPortal esPortal= JSON.parseObject(sourceAsString, EsPortal.class);
+//        //构建改的hashmap
+//        Map<String, Object> jsonMap = new HashMap<>();
+//        jsonMap.put("n_pubs", esPortal.getN_pubs() + num);
+//        //构建updateRequest
+//        UpdateRequest updateRequest = new UpdateRequest("sheep-scholar", "_doc", portal_id);
+//        updateRequest.doc(jsonMap);
+//        UpdateResponse updateResponse = highLevelClient.update(updateRequest,RequestOptions.DEFAULT);
+//    }
 }
